57b9870ab52160eaa65bba05e2b77a9dffe2278f
Alex Shcherbyna <Alex_181173@list.ru>
2025-10-02 15:35:39 +0300

    project in progress

--- main.py
+++ main.py
@@ -7,9 +7,9 @@ HTML_PAGE = """
 <!DOCTYPE html>
 <html lang="ru">
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>Контакты</title>
+  <meta chae="viewport" content="width=device-width, initial-scale=1">
+  <title>rset="UTF-8">
+  <meta namКонтакты</title>
   <!-- Bootstrap CSS -->
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
   <style>
